860cb0e2e3ff719b4e9af74e6ddf5d86b511dad7
lWaku <61800598+lWaku@users.noreply.github.com>
2020-09-07 08:13:35 -0300

    Atividade Yoshi

---
+++ atividade01_leticia_waku/src/atividade01_leticia_waku/Barra.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade01_leticia_waku;
+import java.awt.Color;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ *
+ * @author frawn
+ */
+public class Barra {
+        Double leiMax = 0.0;
+        Double leiMin = 0.0;
+        Double leiTotal = 0.0;
+        Double nLei = 0.0;
+    
+}
